923d313e1c63ea5ccd933e3ed4d3dd0f1aec09e7
Oliver <oliverlanger@gmx.de>
2012-12-01 16:24:33 +0100

    current state

--- app.js
+++ app.js
@@ -2,6 +2,16 @@
 
 var ZeitApp = angular.module('ZeitApp', []);
 
+ZeitApp.config(
+    ['$routeProvider', function($routeProvider) {
+        $routeProvider.
+            when('/', {templateUrl: 'views/select.html',   controller: ZeitCtrl});
+    }
+])
+
 function ZeitCtrl($scope) {
 
+    $scope.articleFind = function() {
+      console.log( "ping" )
+    }
 }

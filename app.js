91078f5e28f8c069e893b765e366a5528b7a3ecb
olli <oliver.zeigermann@gmail.com>
2012-12-01 16:23:36 +0100

    Jau!

--- app.js
+++ app.js
@@ -5,3 +5,24 @@ var ZeitApp = angular.module('ZeitApp', []);
 function ZeitCtrl($scope) {
 
 }
+
+angular.module('mongolab', ['ngResource']).
+    factory('Project', function($resource) {
+        var Project = $resource('http://api.zeit.de/content?q=:' +
+            '/angularjs/collections/projects/:id',
+            { apiKey: '4f847ad3e4b08a2eed5f3b54',  }, {
+                update: { method: 'GET' }
+            }
+        );
+
+        Project.prototype.update = function(cb) {
+            return Project.update({id: this._id.$oid},
+                angular.extend({}, this, {_id:undefined}), cb);
+        };
+
+        Project.prototype.destroy = function(cb) {
+            return Project.remove({id: this._id.$oid}, cb);
+        };
+
+        return Project;
+    });

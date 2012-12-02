57b7fbf3b4da258a22614bf454140559a6aea4f6
olli <oliver.zeigermann@gmail.com>
2012-12-02 11:51:36 +0100

    Added query for details

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 "use strict";
 
 var ZeitApp = angular.module('ZeitApp', ['ng']).config(['$httpProvider', function (http) {
-    delete http.defaults.headers.common['X-Requested-With'];
-}]).factory('Author', function ($http) {
-        var Author = {
+        delete http.defaults.headers.common['X-Requested-With'];
+    }]).factory('Content', function ($http) {
+        var Content = {
             get:function (params, cb) {
                 var url = 'http://api.zeit.de/content?q=*' + params.id.toLowerCase() + '*&limit=10';
                 $http.get(url, {
-                    headers:{'X-Authorization':'602c992cc45dd61c013925c253777e31447df5b2ea6e83152283'}
+                    headers:{'X-Authorization':'b56eb8f8068394f30c303747f837e46f6bf265c18b80d8dc0743'}
                 }).success(function(data, status, headers, config) {
                         cb(data);
                     }).error(function(data, status, headers, config) {
@@ -16,7 +16,21 @@ var ZeitApp = angular.module('ZeitApp', ['ng']).config(['$httpProvider', functio
             }
         };
 
-        return Author;
+        return Content;
+    }).factory('Details', function ($http) {
+        var Details = {
+            get:function (uri, cb) {
+                $http.get(uri, {
+                    headers:{'X-Authorization':'b56eb8f8068394f30c303747f837e46f6bf265c18b80d8dc0743'}
+                }).success(function(data, status, headers, config) {
+                        cb(data);
+                    }).error(function(data, status, headers, config) {
+                        cb(data);
+                    });
+            }
+        };
+
+        return Details;
     });
 
 ZeitApp.config(
@@ -24,28 +38,38 @@ ZeitApp.config(
         $routeProvider.
             when('/', {templateUrl:'views/select.html', controller:ZeitCtrl})
             .when( '/articles', {templateUrl: "views/articles.html", controller:ZeitCtrl})
-            .when( '/article/details', {templateUrl: "views/articleDetails.html", controller: ZeitCtrl});
+            .when( '/article/details', {templateUrl: "views/details.html", controller: ZeitCtrl});
     }
     ])
 
 
-function ZeitCtrl($scope, Author, $location) {
+function ZeitCtrl($scope, Content, Details, $location) {
     $scope.articleFind = function (keyword) {
       $scope.searchPerformed = true;
-      Author.get({id: keyword}, function(articleList) {
+      Content.get({id: keyword}, function(articleList) {
           $scope.$parent.articles = articleList.matches;
           if ($scope.itemsFound()) {
               $location.path('/articles');
           }
-      })
+      });
     }
 
     $scope.itemsFound = function () {
         return !$scope.searchPerformed || ($scope.$parent.articles && $scope.$parent.articles.length != 0);
     }
+
+    $scope.details = function(article) {
+        var uri = article.uri;
+        Details.get(uri, function(details) {
+            $scope.$parent.details = details;
+            $location.path('/article/details');
+        });
+    }
+}
+
+function ArticlesCtrl ($scope) {
 }
 
 
-
-function ArticlesCtrl ($scope ) {
+function DetailsCtrl ($scope) {
 }
